b6f52fba8de778ea7550894b9cf236a72ce4167b
sam <samuelabaltz@gmail.com>
2023-06-20 09:35:36 -0600

    Install material ui

--- DentistWatch/client/src/App.js
+++ DentistWatch/client/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, Routes} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                {/*<Route exact path="/" element={<RecordList />} />*/}
-                {/*<Route path="/edit/:id" element={<Edit />} />*/}
-                {/*<Route path="/create" element={<Create />} />*/}
-            </Routes>
-        </BrowserRouter>
+        <>
+            <NavBar/>
+            <BrowserRouter>
+                <Routes>
+                    {/*<Route exact path="/" element={<RecordList />} />*/}
+                    {/*<Route path="/edit/:id" element={<Edit />} />*/}
+                    {/*<Route path="/create" element={<Create />} />*/}
+                </Routes>
+            </BrowserRouter>
+            <Footer/>
+        </>
     );
 }
 
