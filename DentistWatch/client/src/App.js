ca66a7bce53259dc8cd98a91d60a53631767c62c
sam <samuelabaltz@gmail.com>
2023-06-20 09:29:31 -0600

    Add client slide files

--- DentistWatch/client/src/App.js
+++ DentistWatch/client/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
     return (
-        <div></div>
+        <BrowserRouter>
+            <Routes>
+                {/*<Route exact path="/" element={<RecordList />} />*/}
+                {/*<Route path="/edit/:id" element={<Edit />} />*/}
+                {/*<Route path="/create" element={<Create />} />*/}
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- DentistWatch/server/routes/record.js
+++ DentistWatch/server/routes/record.js
@@ -7,14 +7,14 @@ const router = express.Router();
 
 // This section will help you get a list of all the records.
 router.get("/", async (req, res) => {
-    let collection = await db.collection("records");
+    let collection = await db.collection("dentists");
     let results = await collection.find({}).toArray();
     res.send(results).status(200);
 });
 
 // This section will help you get a single record by id
 router.get("/:id", async (req, res) => {
-    let collection = await db.collection("records");
+    let collection = await db.collection("dentists");
     let query = {_id: new ObjectId(req.params.id)};
     let result = await collection.findOne(query);
 
@@ -26,10 +26,11 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
     let newDocument = {
         name: req.body.name,
-        position: req.body.position,
-        level: req.body.level,
+        location: req.body.location,
+        practice: req.body.practice,
+        complaint: req.body.complaint,
     };
-    let collection = await db.collection("records");
+    let collection = await db.collection("dentists");
     let result = await collection.insertOne(newDocument);
     res.send(result).status(204);
 });
@@ -40,12 +41,13 @@ router.patch("/:id", async (req, res) => {
     const updates =  {
         $set: {
             name: req.body.name,
-            position: req.body.position,
-            level: req.body.level
+            location: req.body.location,
+            practice: req.body.practice,
+            complaint: req.body.complaint,
         }
     };
 
-    let collection = await db.collection("records");
+    let collection = await db.collection("dentists");
     let result = await collection.updateOne(query, updates);
 
     res.send(result).status(200);
@@ -55,7 +57,7 @@ router.patch("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
     const query = { _id: new ObjectId(req.params.id) };
 
-    const collection = db.collection("records");
+    const collection = db.collection("dentists");
     let result = await collection.deleteOne(query);
 
     res.send(result).status(200);
