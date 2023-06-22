82e525b012b587a42e1f415e5eaf1db66ab03ff5
sam <samuelabaltz@gmail.com>
2023-06-22 11:37:16 -0600

    Commit before component library

--- common/Application.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default function Application() {
-    return (
-        <div>test</div>
-    )
-}
--- dentist-watch/src/App.tsx
+++ dentist-watch/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Application from '../../common/Application'
+import {Application} from 'watch-common/export'
 function App() {
   return (
     <Application></Application>
