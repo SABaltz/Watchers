2824e68df2d4fa6d5e6e30592ae9ad57c2362469
sam <samuelabaltz@gmail.com>
2023-06-22 10:49:39 -0600

    Update project to use typescript

--- DentistWatch/client/src/components/HomePage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Box from "@mui/material/Box";
-
-export default function HomePage() {
-    return (
-        <Box sx={{height: '3000px', backgroundColor: 'red'}}></Box>
-    )
-}
