5ff6660810b684c37c41a3628b9854b636f7bbca
sam <samuelabaltz@gmail.com>
2023-06-26 11:56:51 -0600

    add wouter

--- common-watch/src/components/Application.tsx
+++ common-watch/src/components/Application.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 import HomePage from "./HomePage";
@@ -9,6 +8,7 @@ import Search from "./Search";
 import Map from "./Map";
 import {dentistTheme, doctorTheme} from "./styles/theme";
 import {ThemeProvider} from "@mui/material";
+import {Route} from "wouter";
 
 export default function Application({context}) {
 
@@ -16,17 +16,24 @@ export default function Application({context}) {
         <>
             <ThemeProvider theme={context.projectName === "Doctor Watch" ? doctorTheme : dentistTheme}>
                 <NavBar context={context}/>
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<HomePage context={context}/>}/>
-                        <Route path="/report" element={<Report/>}/>
-                        <Route path="/map" element={<Map/>}/>
-                        <Route path="/search" element={<Search/>}/>
-                        <Route path="/list" element={<List/>}/>
-                    </Routes>
-                </BrowserRouter>
+                {/*<BrowserRouter>*/}
+                {/*    <Routes>*/}
+                {/*        <Route path=":context" element={<HomePageWrapper/>}/>*/}
+                {/*        <Route path="/report" element={<Report/>}/>*/}
+                {/*        <Route path="/map" element={<Map/>}/>*/}
+                {/*        <Route path="/search" element={<Search/>}/>*/}
+                {/*        <Route path="/list" element={<List/>}/>*/}
+                {/*    </Routes>*/}
+                {/*</BrowserRouter>*/}
+                <Route path="/"><HomePage context={context}/></Route>
+                <Route path="/report"><Report/></Route>
+                <Route path="/map"><Map/></Route>
+                <Route path="/search"><Search/></Route>
+                <Route path="/list"><List/></Route>
                 <Footer context={context}/>
             </ThemeProvider>
         </>
     )
 }
+
+
--- common-watch/src/components/HomePage.tsx
+++ common-watch/src/components/HomePage.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Box from "@mui/material/Box";
 
 let dentist1 = require('../static/dentist1.jpg')
+let doctor1 = require('../static/doctor1.jpg')
+
 export default function HomePage({context}) {
+    console.log(context)
     return (
         <>
             <Box
                 sx={{
-                    backgroundImage: 'url(' + dentist1 + ')',
+                    // backgroundImage: 'url(' + context.projectName === "Dentist Watch" ? dentist1 : doctor1 + ')',
                     backgroundRepeat: "no-repeat",
                     backgroundPosition: 'center',
                     backgroundSize: 'cover',
